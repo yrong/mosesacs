48c1f242266088e31531a3cf113b3bedb076ec33
Luca Cervasio <luca.cervasio@gmail.com>
2014-05-20 13:44:05 +0200

    read/write from ws async

--- client/api.go
+++ client/api.go
@@ -3,14 +3,12 @@ package client
 import (
         "fmt"
         "log"
-
         "code.google.com/p/go.net/websocket"
 )
 
 // This example demonstrates a trivial client.
-func ExampleDial() {
+func Connect(url string) {
         origin := "http://localhost/"
-        url := "ws://localhost:9292/api"
         ws, err := websocket.Dial(url, "", origin)
         if err != nil {
                 log.Fatal(err)
@@ -18,10 +16,14 @@ func ExampleDial() {
         if _, err := ws.Write([]byte("hello, world!\n")); err != nil {
                 log.Fatal(err)
         }
+        log.Println("sent")
         var msg = make([]byte, 512)
         var n int
-        if n, err = ws.Read(msg); err != nil {
-                log.Fatal(err)
+        fmt.Println("waiting to read...")
+        for {
+          if n, err = ws.Read(msg); err != nil {
+                  log.Fatal(err)
+          }
+          fmt.Printf("Received: %s.\n", msg[:n])
         }
-        fmt.Printf("Received: %s.\n", msg[:n])
 }
