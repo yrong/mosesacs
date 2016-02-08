285c03e3d5487bfecebae3b437d83622a24aa3b1
Luca Cervasio <luca.cervasio@gmail.com>
2016-02-08 10:44:06 +0100

    mib example files

---
+++ client/mib.go
@@ -0,0 +1,33 @@
+package cwmpclient
+
+import(
+  "fmt"
+)
+
+type MibValue struct {
+  
+}
+
+type Mib struct {
+  Tree map[string]string
+}
+
+
+
+func NewMib() *Mib {
+ fmt.Println("creating new struct")
+ m := &Mib{}
+ return m
+}
+
+func (mib *Mib) AddSubTree(path string) {
+
+}
+
+func (mib *Mib) GetValue(path string) {
+
+}
+
+func (mib *Mib) SetValue(path string, value string) {
+
+}
---
+++ example/mib_ex.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "github.com/lucacervasio/mosesacs/client"
+)
+
+func main() {
+  m := cwmpclient.NewMib()
+  fmt.Println(m)
+}
