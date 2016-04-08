f14b73cb9ea90331a89db81252d2da061ef1c13f
Luca Cervasio <luca.cervasio@gmail.com>
2016-04-08 13:54:59 +0200

    go fmt

--- xmpp/xmpp.go
+++ xmpp/xmpp.go
@@ -1,10 +1,10 @@
 package xmpp
 
 import (
-	"github.com/tsuibin/goxmpp2/xmpp"
 	"crypto/tls"
 	"encoding/xml"
-//	"flag"
+	"github.com/tsuibin/goxmpp2/xmpp"
+	//	"flag"
 	"fmt"
 	"log"
 	//"os"
@@ -14,16 +14,16 @@ import (
 var c *xmpp.Client
 
 func StartClient(user string, pwd string, cb func(string)) {
-  /*
-	jidStr := flag.String("jid", "", "JID to log in as")
-	pw := flag.String("pw", "", "password")
-	flag.Parse()
-	jid := xmpp.JID(*jidStr)
-	if jid.Domain() == "" || *pw == "" {
-		flag.Usage()
-		os.Exit(2)
-	}
-  */
+	/*
+		jidStr := flag.String("jid", "", "JID to log in as")
+		pw := flag.String("pw", "", "password")
+		flag.Parse()
+		jid := xmpp.JID(*jidStr)
+		if jid.Domain() == "" || *pw == "" {
+			flag.Usage()
+			os.Exit(2)
+		}
+	*/
 
 	jid := xmpp.JID(user)
 
@@ -34,7 +34,7 @@ func StartClient(user string, pwd string, cb func(string)) {
 		}
 	}()
 	tlsConf := tls.Config{InsecureSkipVerify: true}
-  var err error
+	var err error
 	c, err = xmpp.NewClient(&jid, pwd, tlsConf, nil, xmpp.Presence{}, stat)
 	if err != nil {
 		log.Fatalf("NewClient(%v): %v", jid, err)
@@ -44,83 +44,80 @@ func StartClient(user string, pwd string, cb func(string)) {
 	go func(ch <-chan xmpp.Stanza) {
 		for obj := range ch {
 			fmt.Printf("s: %v\n", obj)
-      str := fmt.Sprintf("%v", obj)
-      cb(str)
+			str := fmt.Sprintf("%v", obj)
+			cb(str)
 		}
 		fmt.Println("done reading")
 	}(c.Recv)
 
+	/*
+		roster := c.Roster.Get()
+		fmt.Printf("%d roster entries:\n", len(roster))
+		for i, entry := range roster {
+			fmt.Printf("%d: %v\n", i, entry)
+		}
+	*/
 
- /*
-	roster := c.Roster.Get()
-	fmt.Printf("%d roster entries:\n", len(roster))
-	for i, entry := range roster {
-		fmt.Printf("%d: %v\n", i, entry)
-	}
-*/
-
-/*
-	p := make([]byte, 1024)
-	for {
-		nr, _ := os.Stdin.Read(p)
-		if nr == 0 {
-			break
+	/*
+		p := make([]byte, 1024)
+		for {
+			nr, _ := os.Stdin.Read(p)
+			if nr == 0 {
+				break
+			}
+			s := string(p)
+			dec := xml.NewDecoder(strings.NewReader(s))
+			t, err := dec.Token()
+			if err != nil {
+				fmt.Printf("token: %s\n", err)
+				break
+			}
+			var se *xml.StartElement
+			var ok bool
+			if se, ok = t.(*xml.StartElement); !ok {
+				fmt.Println("Couldn't find start element")
+				break
+			}
+			var stan xmpp.Stanza
+			switch se.Name.Local {
+			case "iq":
+				stan = &xmpp.Iq{}
+			case "message":
+				stan = &xmpp.Message{}
+			case "presence":
+				stan = &xmpp.Presence{}
+			default:
+				fmt.Println("Can't parse non-stanza.")
+				continue
+			}
+			err = dec.Decode(stan)
+			if err == nil {
+				c.Send <- stan
+			} else {
+				fmt.Printf("Parse error: %v\n", err)
+				break
+			}
 		}
-		s := string(p)
-		dec := xml.NewDecoder(strings.NewReader(s))
-		t, err := dec.Token()
-		if err != nil {
-			fmt.Printf("token: %s\n", err)
-			break
-		}
-		var se *xml.StartElement
-		var ok bool
-		if se, ok = t.(*xml.StartElement); !ok {
-			fmt.Println("Couldn't find start element")
-			break
-		}
-		var stan xmpp.Stanza
-		switch se.Name.Local {
-		case "iq":
-			stan = &xmpp.Iq{}
-		case "message":
-			stan = &xmpp.Message{}
-		case "presence":
-			stan = &xmpp.Presence{}
-		default:
-			fmt.Println("Can't parse non-stanza.")
-			continue
-		}
-		err = dec.Decode(stan)
-		if err == nil {
-			c.Send <- stan
-		} else {
-			fmt.Printf("Parse error: %v\n", err)
-			break
-		}
-	}
-	fmt.Println("done sending")
-  */
+		fmt.Println("done sending")
+	*/
 }
 
-
 func SendConnectionRequest(cpe string) {
-  outmsg := `<iq from="acs@mosesacs.org" to="`+cpe+`" id="cr001" type="get"><connectionRequest xmlns="urn:broadband-forum-org:cwmp:xmppConnReq-1-0"><username>username</username><password>password</password></connectionRequest></iq>`
- dec := xml.NewDecoder(strings.NewReader(outmsg))
-  var stan xmpp.Stanza
-  stan = &xmpp.Iq{}
-  err := dec.Decode(stan)
-  if err == nil {
-    fmt.Println(stan)
-    c.Send <- stan
-  } else {
-    fmt.Printf("Parse error: %v\n", err)
-  }
-
+	outmsg := `<iq from="acs@mosesacs.org" to="` + cpe + `" id="cr001" type="get"><connectionRequest xmlns="urn:broadband-forum-org:cwmp:xmppConnReq-1-0"><username>username</username><password>password</password></connectionRequest></iq>`
+	dec := xml.NewDecoder(strings.NewReader(outmsg))
+	var stan xmpp.Stanza
+	stan = &xmpp.Iq{}
+	err := dec.Decode(stan)
+	if err == nil {
+		fmt.Println(stan)
+		c.Send <- stan
+	} else {
+		fmt.Printf("Parse error: %v\n", err)
+	}
 
 }
 
 func Close() {
-  fmt.Println("closing xmpp client")
-  c.Close()
+	fmt.Println("closing xmpp client")
+	c.Close()
 }
