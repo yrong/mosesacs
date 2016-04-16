fb6a062f1203cbe3b26a893b26b58d7ec2333885
Luca Cervasio <luca.cervasio@gmail.com>
2016-04-16 13:58:33 +0200

    compilation script sets public permissions

--- build.sh
+++ build.sh
@@ -6,40 +6,40 @@ GOOS=linux GOARCH=386 go build -o bin/mosesacs mosesacs.go
 cd bin
 tar cvfz mosesacs_${VER}_linux_i386.tar.gz mosesacs
 rm mosesacs
-s3cmd -c ~/.s3cfg put mosesacs_${VER}_linux_i386.tar.gz s3://mosesacs/
+s3cmd -c ~/.s3cfg -P put mosesacs_${VER}_linux_i386.tar.gz s3://mosesacs/
 cd ..
 
 GOOS=linux GOARCH=amd64 go build -o bin/mosesacs mosesacs.go
 cd bin
 tar cvfz mosesacs_${VER}_linux_amd64.tar.gz mosesacs
 rm mosesacs
-s3cmd -c ~/.s3cfg put mosesacs_${VER}_linux_amd64.tar.gz s3://mosesacs/
+s3cmd -c ~/.s3cfg -P put mosesacs_${VER}_linux_amd64.tar.gz s3://mosesacs/
 cd ..
 
 GOOS=linux GOARCH=arm go build -o bin/mosesacs mosesacs.go
 cd bin
 tar cvfz mosesacs_${VER}_arm.tar.gz mosesacs
 rm mosesacs
-s3cmd -c ~/.s3cfg put mosesacs_${VER}_arm.tar.gz s3://mosesacs/
+s3cmd -c ~/.s3cfg -P put mosesacs_${VER}_arm.tar.gz s3://mosesacs/
 cd ..
 
 GOOS=darwin GOARCH=amd64 go build -o bin/mosesacs mosesacs.go
 cd bin
 tar cvfz mosesacs_${VER}_osx_amd64.tar.gz mosesacs
 rm mosesacs
-s3cmd -c ~/.s3cfg put mosesacs_${VER}_osx_amd64.tar.gz s3://mosesacs/
+s3cmd -c ~/.s3cfg -P put mosesacs_${VER}_osx_amd64.tar.gz s3://mosesacs/
 cd ..
 
 GOOS=windows GOARCH=386 go build -o bin/mosesacs.exe mosesacs.go
 cd bin
-tar cvfz mosesacs_${VER}_win_i386.tar.gz mosesacs.exe
+zip mosesacs_${VER}_win_i386.zip mosesacs.exe
 rm mosesacs.exe
-s3cmd -c ~/.s3cfg put mosesacs_${VER}_win_i386.tar.gz s3://mosesacs/
+s3cmd -c ~/.s3cfg -P put mosesacs_${VER}_win_i386.zip s3://mosesacs/
 cd ..
 
 GOOS=windows GOARCH=amd64 go build -o bin/mosesacs.exe mosesacs.go
 cd bin
-tar cvfz mosesacs_${VER}_win_amd64.tar.gz mosesacs.exe
+zip mosesacs_${VER}_win_amd64.zip mosesacs.exe
 rm mosesacs.exe
-s3cmd -c ~/.s3cfg put mosesacs_${VER}_win_amd64.tar.gz s3://mosesacs/
+s3cmd -c ~/.s3cfg -P put mosesacs_${VER}_win_amd64.zip s3://mosesacs/
 cd ..
